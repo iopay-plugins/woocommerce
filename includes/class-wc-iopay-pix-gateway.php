a68e0ff1c9277e84117bcd78ab84679a214fefbf
emanuellopess <lopes.emanuel@linknacional.com>
2023-05-04 20:47:30 +0000

    fix: Correção de bug de script de PIX não ser carregado caso o PIX seja a única opção de pagamento

--- includes/class-wc-iopay-pix-gateway.php
+++ includes/class-wc-iopay-pix-gateway.php
@@ -49,6 +49,7 @@ class WC_Iopay_Pix_Gateway extends Wc_Iopay_Paymethod_Gateway {
         $this->api = new WC_Iopay_API($this);
 
         // Actions.
+        add_action('wp_enqueue_scripts', array($this, 'checkout_scripts'));
         add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
         add_action('woocommerce_thankyou_' . $this->id, array($this, 'thankyou_page'));
         add_action('woocommerce_email_after_order_table', array($this, 'email_instructions'), 10, 3);
@@ -144,6 +145,17 @@ class WC_Iopay_Pix_Gateway extends Wc_Iopay_Paymethod_Gateway {
                 'description' => sprintf(__('Log Iopay events, such as API requests. You can check the log in %s', 'woocommerce-iopay'), '<a href="' . esc_url(admin_url('admin.php?page=wc-status&tab=logs&log_file=' . esc_attr($this->id) . '-' . sanitize_file_name(wp_hash($this->id)) . '.log')) . '">' . __('System Status &gt; Logs', 'woocommerce-iopay') . '</a>'),
             ),
         );
+    }
+
+    /**
+     * Define script load for frontend PIX.
+     *
+     * @since 1.1.0
+     */
+    public function checkout_scripts() {
+        if (is_checkout()) {
+            wp_enqueue_script('iopay-pix', plugins_url('assets/js/pix.js', plugin_dir_path(__FILE__)), array('jquery'), date('is'), true);
+        }
     }
 
     /**
